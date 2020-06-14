4d4672134ed65e959f8e0e0bc177a30565236ff0
HaraHiroshi <mrbob403@gmail.com>
2020-06-14 23:17:20 +0900

    some validation error resolved.

--- js/build/app.js
+++ js/build/app.js
@@ -23,15 +23,16 @@ var _schema2 = _interopRequireDefault(_schema);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // 表データ読み込み
-var data = JSON.parse(localStorage.getItem('data'));
-
-// localStrageに保存されているデータがない場合、サンプルデータを読み込む
-if (!data) {
-  data = {};
+var localdata = localStorage.getItem('data');
+var data = void 0;
+if (localdata) {
+  data = JSON.parse(localdata);
+} else {
+  // localStrageに保存されているデータがない場合、サンプルデータを読み込む
+  data = [{}];
   _schema2.default.forEach(function (item) {
-    data[item.id] = item.sample;
+    data[0][item.id] = item.sample;
   });
-  data = [data];
 }
 
 _reactDom2.default.render(_react2.default.createElement(
