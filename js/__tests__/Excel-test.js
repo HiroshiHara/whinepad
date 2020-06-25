2caa2023a85a73b25a49e7b58709e9d5c69068b3
HaraHiroshi <mrbob403@gmail.com>
2020-06-25 23:33:41 +0900

    create Excel-test.js.

---
+++ js/__tests__/Excel-test.js
@@ -0,0 +1,41 @@
+jest.disableAutomock();
+
+import React from 'react';
+import TestUtils from 'react-dom/test-utils';
+import Excel from '../source/components/Excel';
+import schema from '../source/schema';
+
+let data = [{}];
+schema.forEach(item => data[0][item.id] = item.sample);
+
+describe('1. Editing data.', () => {
+  it('1-1. create new data.', () => {
+    // prepare onDataChange mock.
+    const onDataChangeMock = jest.fn();
+    // rendering Excel.
+    const excel = TestUtils.renderIntoDocument(
+      <Excel
+        schema={schema}
+        initialData={data}
+        onDataChange={onDataChangeMock}
+      />
+    )
+
+    // modify name.
+    const newname = 'Pescado branc';
+    const cell = TestUtils.scryRenderedDOMComponentsWithTag(excel, 'td')[0];
+    // cell.dataset = {
+    //   row: cell.getAttribute('data-row'),
+    //   key: cell.getAttribute('data-key')
+    // };
+    TestUtils.Simulate.doubleClick(cell);
+    cell.getElementsByTagName('input')[0].value = newname;
+    TestUtils.Simulate.submit(cell.getElementsByTagName('form')[0]);
+    // after modified name verify.
+    expect(cell.innerHTML).toEqual(newname);
+    // submitted cell info verify.
+    const calls = onDataChangeMock.mock.calls;
+    console.log(calls[0][0]);
+    expect(calls[0][0][0].name).toEqual(newname);
+  })
+})
