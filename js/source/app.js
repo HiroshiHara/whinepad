c81bc9c1852663402dc7e1010338f9e5dccbfa84
HaraHiroshi <mrbob403@gmail.com>
2020-07-04 17:52:17 +0900

    create some js-file for flux.

--- js/source/app.js
+++ js/source/app.js
@@ -7,26 +7,17 @@ import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
 import Whinepad from './components/Whinepad';
 import schema from './schema';
+import CRUDStore from './flux/CRUDStore';
 
-// 表データ読み込み
-const localdata = localStorage.getItem('data');
-let data;
-if (localdata) {
-  data = JSON.parse(localdata);
-} else {
-  // localStrageに保存されているデータがない場合、サンプルデータを読み込む
-  data = [{}];
-  schema.forEach(item => {
-    data[0][item.id] = item.sample
-  });
-}
+// initialize the data of application.
+CRUDStore.init(schema);
 
 ReactDOM.render(
   <div>
     <div className="app-header">
       <Logo /> Whinepad
     </div>
-    <Whinepad schema={schema} initialData={data}></Whinepad>
+    <Whinepad></Whinepad>
   </div>,
   document.getElementById('pad')
 );
---
+++ js/source/flux/CRUDStore.js
@@ -0,0 +1,58 @@
+/* @flow */
+
+let data;
+let schema;
+
+const CRUDStore = {
+  getData(): Array<Object> {
+    return data;
+  },
+  getSchema(): Array<Object> {
+    return schema;
+  },
+  setData(newData: Array<Object>, commit: boolean = true) {
+    data = newData;
+    if (commit && 'localStrage' in window) {
+      localStorage.setItem('data', JSON.stringify(newData));
+    }
+    emitter.emit('change');
+  },
+
+  /**
+   * Get record count.
+   * @returns record count
+   */
+  getCount(): number {
+    return data.length;
+  },
+
+  /**
+   * Get a record associated with the ID of an argument.
+   * @param {number} recordId
+   * @returns a record or null
+   */
+  getRecord(recordId: number): ?Object {
+    return recordId in data ? data[recordId] : null;
+  },
+
+  /**
+   * Initialize Application Data.
+   * If the data is already stored in LocalStrage, load and set it.
+   * Else if, set a data of sample.
+   * @param {Array<Object>} initialSchema
+   */
+  init(initialSchema: Array<Object>) {
+    schema = initialSchema;
+    const storage = 'localStrage' in window
+      ? localStorage.getItem('data')
+      : null;
+    if (!storage) {
+      data = [{}];
+      schema.forEach(item => data[0][item.id] = item.sample);
+    } else {
+      data = JSON.parse(storage);
+    }
+  }
+}
+
+export default CRUDStore
